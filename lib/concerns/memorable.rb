df5cb442c3d8d56cf75e57b239aa80d98a5b5ca2
Noahkimtai <noahkimtai24@gmail.com>
2022-10-31 18:21:50 +0300

    Completed assignment

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,11 @@
+## create a module that will contain all the common methods for artist and song
+module Memorable 
+
+    def reset_all
+        all.clear
+    end
+    def count
+        all.count
+    end
+
+end
